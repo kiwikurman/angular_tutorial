d8666c042b0ba7208442e837ba11b46fa04a8925
abc <abc@abc.com>
2018-04-02 14:58:39 +0300

    list + master-detail + ngIF

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,12 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+{id: 11, name: 'superman'},
+{id: 12, name: 'iceman'},
+{id: 13, name: 'dr evil'},
+{id: 14, name: 'tom waits'},
+{id: 15, name: 'bruce springsteen'},
+{id: 16, name: 'm ward'},
+{id: 17, name: 'perl jam'},
+{id: 18, name: 'mark knofler'}
+];
